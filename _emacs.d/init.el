952750e0f9039e6988162b55d097185cba33ba5a
David Zderic <dzderic@gmail.com>
2012-10-01 12:22:21 +1000

    Adding puppet-mode and ssh-config-mode

--- _emacs.d/init.el
+++ _emacs.d/init.el
@@ -10,14 +10,18 @@
 ;; Packages to be installed:
 (defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings
                       starter-kit-js starter-kit-ruby starter-kit-eshell
-                      color-theme-solarized markdown-mode)
+                      color-theme-solarized markdown-mode ssh-config-mode
+                      puppet-mode)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
 
+;; Enable solarized and make it work
 (load-theme 'solarized-dark t)
-
 (custom-set-faces
  '(esk-paren-face ((t (:foreground "red")))))
+
+;; Mapping of various file extensions to modes
+(add-to-list 'auto-mode-alist '("\.pp$" . puppet-mode))
