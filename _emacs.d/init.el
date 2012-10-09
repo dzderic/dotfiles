92a21584a0ff66feb8effeb1f11b56fc84a0c879
David Zderic <dzderic@gmail.com>
2012-10-10 09:25:26 +1100

    Requiring 'hippie-expand so emacs doesn't die

--- _emacs.d/init.el
+++ _emacs.d/init.el
@@ -1,3 +1,6 @@
+;; Require 'hippie-expand or everything dies
+(require 'hippie-exp)
+
 (require 'package)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
