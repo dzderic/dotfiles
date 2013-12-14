bacbf4ba25620b80bcd615146804d387e579e25c
David Zderic <dzderic@gmail.com>
2013-12-14 12:47:47 +0000

    Fix IPython configuration

--- _ipython/profile_default/ipython_notebook_config.py
+++ _ipython/profile_default/ipython_notebook_config.py
@@ -5,14 +5,13 @@ import sys
 root = lambda x: os.path.join(os.path.dirname(inspect.currentframe().f_code.co_filename), x)
 sys.path.append(root('.'))
 
-def do_config(c):
-    c.IPKernelApp.pylab = "inline"
+c = get_config()
 
-    c.NotebookApp.ip = "*"
-    c.NotebookApp.open_browser = False
-    c.NotebookApp.port = 8888
+c.IPKernelApp.pylab = "inline"
 
-    c.NotebookApp.certfile = root('cert.pem')
-    c.NotebookApp.password = u"sha1:cd51c7528f1a:0be96ea5f99cc9930f75b0490611864e5234767e"
+c.NotebookApp.ip = "*"
+c.NotebookApp.open_browser = False
+c.NotebookApp.port = 8888
 
-    c.NotebookApp.notebook_manager_class = "ipython_notebook_config.GitNotebookManager"
+c.NotebookApp.certfile = root('cert.pem')
+c.NotebookApp.password = u"sha1:cd51c7528f1a:0be96ea5f99cc9930f75b0490611864e5234767e"
